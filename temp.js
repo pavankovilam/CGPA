292d871b1764b6e9e5d55105c57664e6c71084a6
Pavan Kumar Kovilampati <113405546+pavankovilam@users.noreply.github.com>
2024-08-09 16:44:10 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,12 @@
+var agent = navigator.userAgent;
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.getElementById("device").value = agent;
+document.getElementById("test").value =(response.ip).toString();
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyYIvcJBwIvfOSN530tjgyxhGZMC4S789PmSi_Tc_tmWnGndBYHqAqw3DwW6THBF1wVPQ/exec'
+const form = document.forms['Inputer']
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
